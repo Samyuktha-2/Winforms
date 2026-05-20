b92d0e6f46d2f03a7b18c35fe8662c23bd94806a
Samyuktha S <115525167+Samyuktha-2@users.noreply.github.com>
2026-05-20 15:48:52 +0530

    LINQ query practice - console project

---
+++ LINQ/LINQ/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LINQ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LINQ")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("10737237-ec9b-45c3-8289-66c1c7142973")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LINQ/LINQ/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace LINQ
+{
+    class Student
+    { 
+        public string StudentName { get; set; }
+        public int CourseId { get; set; }
+    }
+
+    class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+    }
+}
